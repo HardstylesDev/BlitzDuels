ec1f7754274c814c260577bbf6fc136081008145
Carlos Ramales <112786593+Rawcores@users.noreply.github.com>
2022-09-30 19:32:55 +0200

    Update database.sql
    Changes
    
    Co-authored-by: @HardstylesDev

--- database.sql
+++ database.sql
@@ -3,6 +3,7 @@ CREATE TABLE IF NOT EXISTS `data` (
   `deaths` int(11) DEFAULT 0,
   `wins` int(11) DEFAULT NULL,
   `streak` int(11) DEFAULT 0,
-  `elo` int(11) DEFAULT NULL
-  `layout` varchar(9999) DEFAULT null
+  `elo` int(11) DEFAULT NULL,
+  `layout` varchar(9999) DEFAULT NULL,
+  `coins` int(11) DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
