32e225becb495947519fcc0e420a73d061a0469e
Carlos Ramales <112786593+Rawcores@users.noreply.github.com>
2022-09-30 19:28:54 +0200

    Create database.sql
    Added the missing sql files 
    
    Co-authored-by: @HardstylesDev

---
+++ database.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS `data` (
+  `kills` int(11) NOT NULL,
+  `deaths` int(11) DEFAULT 0,
+  `wins` int(11) DEFAULT NULL,
+  `streak` int(11) DEFAULT 0,
+  `elo` int(11) DEFAULT NULL
+  `layout` varchar(9999) DEFAULT null
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
